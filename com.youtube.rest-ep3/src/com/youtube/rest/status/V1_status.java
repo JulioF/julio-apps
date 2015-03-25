aa60f6ac156bc6adf9fa0ecafe3c60a0736e1d13
JulioF <fernandezlesmo@gmail.com>
2015-03-25 11:26:26 -0300

    prueba

--- com.youtube.rest-ep3/src/com/youtube/rest/status/V1_status.java
+++ com.youtube.rest-ep3/src/com/youtube/rest/status/V1_status.java
@@ -1,15 +1,14 @@
 package com.youtube.rest.status;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * This is the root path for our restful api service
- * In the web.xml file, we specified that /api/* need to be in the URL to
- * get to this class.
+ * This is the root path for our restful api service In the web.xml file, we specified that /api/* need to be in the URL to get to this class.
  * 
- * We are versioning the class in the URL path.  This is the first version v1.
- * Example how to get to the root of this api resource:
+ * We are versioning the class in the URL path. This is the first version v1. Example how to get to the root of this api resource:
  * http://localhost:7001/com.youtube.rest/api/v1/status
  * 
  * @author 308tube
@@ -18,11 +17,10 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status/*")
 public class V1_status {
 
-	private static final String api_version = "00.01.00"; //version of the api
-	
+	private static final String api_version = "00.01.00"; // version of the api
+
 	/**
-	 * This method sits at the root of the api.  It will return the name
-	 * of this api.
+	 * This method sits at the root of the api. It will return the name of this api.
 	 * 
 	 * @return String - Title of the api
 	 */
@@ -31,14 +29,12 @@ public class V1_status {
 	public String returnTitle() {
 		return "<p>Java Web Service</p>";
 	}
-	
+
 	/**
-	 * This method will return the version number of the api
-	 * Note: this is nested one down from the root.  You will need to add version
-	 * into the URL path.
+	 * This method will return the version number of the api Note: this is nested one down from the root. You will need to add version into the URL
+	 * path.
 	 * 
-	 * Example:
-	 * http://localhost:7001/com.youtube.rest/api/v1/status/version
+	 * Example: http://localhost:7001/com.youtube.rest/api/v1/status/version
 	 * 
 	 * @return String - version number of the api
 	 */
@@ -48,5 +44,5 @@ public class V1_status {
 	public String returnVersion() {
 		return "<p>Version:</p>" + api_version;
 	}
-	
+	// hola
 }
